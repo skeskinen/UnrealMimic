193a6be04632939acec1011316568404a13876f3
Cyrus Cook <11858105+cyruscook@users.noreply.github.com>
2019-02-10 15:49:34 +0000

    Updated to 40.20.3

--- Plugins/UnrealMimic/Source/UnrealMimic/UnrealMimic.Build.cs
+++ Plugins/UnrealMimic/Source/UnrealMimic/UnrealMimic.Build.cs
@@ -2,8 +2,9 @@ using UnrealBuildTool;
 
 public class UnrealMimic : ModuleRules
 {
-    public UnrealMimic(TargetInfo Target)
-	{
+    //public UnrealMimic(TargetInfo Target)
+    public UnrealMimic(ReadOnlyTargetRules Target) : base(Target)
+    {
         PublicDependencyModuleNames.AddRange(
 			new string[] {
 				"Core",
